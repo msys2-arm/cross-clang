53c1332bf99c89ea142a06b2523a81f047e9cc6b
Jeremy Drake <github@jdrake.com>
2021-11-04 17:15:15 -0700

    mingw-w64-cross-clang: wrappers for cross compilation
    This allows using clang to target other Windows arches using prefixed
    cross tools (ie, aarch64-w64-mingw32-clang).

--- mingw-w64-cross-clang/clang-target-wrapper.c
+++ mingw-w64-cross-clang/clang-target-wrapper.c
@@ -59,6 +59,7 @@ int _tmain(int argc, TCHAR* argv[]) {
     if (getenv("CCACHE"))
         exec_argv[arg++] = _T("ccache");
     exec_argv[arg++] = concat(dir, _T(CLANG));
+    exec_argv[arg++] = _T("--start-no-unused-arguments");
 
     // If changing this wrapper, change clang-target-wrapper.sh accordingly.
     if (!_tcscmp(exe, _T("clang++")) || !_tcscmp(exe, _T("g++")) || !_tcscmp(exe, _T("c++")))
@@ -89,13 +90,13 @@ int _tmain(int argc, TCHAR* argv[]) {
     exec_argv[arg++] = _T("-unwindlib=libunwind");
     exec_argv[arg++] = _T("-stdlib=libc++");
     exec_argv[arg++] = _T("-fuse-ld=lld");
-    exec_argv[arg++] = _T("-Qunused-arguments");
+    exec_argv[arg++] = _T("--end-no-unused-arguments");
 
     for (int i = 1; i < argc; i++)
         exec_argv[arg++] = argv[i];
 
     exec_argv[arg] = NULL;
-    if (arg > max_arg) {
+    if (arg >= max_arg) {
         fprintf(stderr, "Too many options added\n");
         abort();
     }
